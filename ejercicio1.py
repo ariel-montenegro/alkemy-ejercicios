a0c8c7938360e4159c712ebd52d14635fe8d0ea8
Ariel Montenegro <ariel.montenegro@mirgor.com>
2023-11-02 09:43:39 -0300

    Se modifica la bicicleta

--- ejercicio1.py
+++ ejercicio1.py
@@ -21,23 +21,7 @@ class Bicicleta:
         self.velocidad = 0
         
 
-class Animal:
-    cantidad_patas = 0
-    vertebrado = True
 
-    def comer():
-        print('Estoy comiendo')
-        
-class Perro(Animal):
-    
-    def __init__(self, nombre, raza) -> None:
-        self.nombre = nombre
-        self.raza = raza
-    
-class Aguila(Animal):
-    
-    def volar():
-        print('estoy volando')
         
 
     
